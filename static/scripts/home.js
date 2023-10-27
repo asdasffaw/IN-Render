eba6c5374b969cfa025da2a33bb3c7fc82ff7a25
asdasffaw <147673902+asdasffaw@users.noreply.github.com>
2023-10-26 19:04:00 -0500

    Update home.js

--- static/scripts/home.js
+++ static/scripts/home.js
@@ -32,7 +32,7 @@ if (!inFrame && !navigator.userAgent.includes("Firefox")) {
 
         doc.head.appendChild(link);
         doc.body.appendChild(iframe)
-        close();
+        window.close();
     }
 }
 
